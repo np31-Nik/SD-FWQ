694961130d1c1485d8c7172a6b8e587150705faf
Serhii Vidernikov <sv54@gcloud.ua.es>
2021-11-02 17:36:10 +0100

    mapa de la bd nueva

--- sql.py
+++ sql.py
@@ -4,30 +4,46 @@ conn = sqlite3.connect('db.db')
 
 c=conn.cursor()
 
-
-c.execute("""CREATE TABLE IF NOT EXISTS usuarios(
-    id text PRIMARY KEY,
-    username text NOT NULL,
-    password text NOT NULL
-    )""")
-
 # c.execute("""CREATE TABLE IF NOT EXISTS atracciones(
 #     id text PRIMARY KEY,
 #     visitantesCiclo int NOT NULL,
 #     tiempoCiclo int NOT NULL
 #     )""")
     
-# c.execute("""CREATE TABLE IF NOT EXISTS Mapa(
-#     id text PRIMARY KEY,
-#     x int NOT NULL,
-#     y int NOT NULL,
-#     valor text,
-#     FOREIGN KEY (valor) REFERENCES atracciones (id)
-#     )""")
-
+c.execute("""CREATE TABLE IF NOT EXISTS Mapa(
+    id text,
+    x int NOT NULL,
+    y int NOT NULL,
+    valor text,
+    PRIMARY KEY (id,x,y),
+    FOREIGN KEY (valor) REFERENCES atracciones (id)
+    )""")
+#c.execute("Drop Table mapa")
 # print(c.execute("""SELECT * from usuarios"""))
 # usuario=c.fetchall()
 # print(usuario[0][1])
 
+#c.execute("""INSERT INTO usuarios (val) VALUES ()"""),
 
+str=""". . . . . . . . . a1 . . . . . . . a14 . .
+\n. . . . . . . . . . . . . . . . . . . .
+\n. . . . . . a6 . . . . . . . . . . . . .
+\n. . . . a7 . . . . . . . . . . . . . . .
+\n. . . . . . a2 . . . . . . . . . . . . .
+\n. . . . . . . . . . . . . . . . . . . .
+\n. . . . . . . . . . . . . . . . . a9 . .
+\n. . . . . . . . . . . . . . . . . . . .
+\n. . . a8 . . . . . . . . . . . . . . . a5
+\n. . . . . . . . . . . . . . . . . . . .
+\n. . . . . . . . . . a15 . . . . . . . . .
+\n. . . . . . . . . . . . . . . . . . . .
+\n. . . . . . . . . . . . . . . . . . . .
+\n. . . . . . . . . . . . . a10 . . . . . .
+\n. . . . . . . . . . . . . . . . . . . .
+\n. . . . . a3 . . . . . . . . . . . . . .
+\n. . . . . . a12 . . . . . . . . . . . . .
+\n. . . . . . . . . . . . . . . . . . . .
+\n. . . . . . . a4 . . . . . . . . . . . .
+\n. . . . . . . . . . . . . . . a13 . . . .\n"""
+print(str)
 conn.commit()
