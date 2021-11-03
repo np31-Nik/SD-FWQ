eeb644ae6627bee12673f0a1419c88af981b45d8
Nikita Polyanskiy <np31@alu.ua.es>
2021-11-03 20:14:51 +0100

    ServidorTiemposEspera

--- WaitingTimeServer/FWQ_WaitingTimeServer.py
+++ WaitingTimeServer/FWQ_WaitingTimeServer.py
@@ -1,12 +1,40 @@
 from concurrent import futures
-
+from kafka import KafkaConsumer
 import logging
 import grpc
-class WaitingTime(object):
-    def WaitingTimeServer(self,request,context):
-		return WaitingTimeServer.WaitingTimeServerResponse(response=calcularTiempo())
+import sys
 
-    def calcularTiempo():
+# class WaitingTime(object):
+#     def WaitingTimeServer(self,request,context):
+# 		return WaitingTimeServer.WaitingTimeServerResponse(response=calcularTiempo())
+
+#     def calcularTiempo():
+
+def escuchaSensor():
+  	print("hola q tal")
+
+def main():
+	if(len(sys.argv) != 4):
+		print("Para ejecutar utiliza: FWQ_WaitingTimeServer.py |PUERTO ESCUCHA| |IP GESTOR| |PUERTO GESTOR|")
+  	else:
+		puerto_escucha = sys.argv[1]
+		ip_gestor = sys.argv[2]
+		puerto_gestor = sys.argv[3]
+		personas = 0
+
+		escuchaSensor()
+
+		#escuchaEngine()
+
+
+
+#------------------------
+if __name__=="__main__":
+    main()
+
+
+
+
 
 
 
