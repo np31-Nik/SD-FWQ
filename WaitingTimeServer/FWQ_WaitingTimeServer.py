8fa2bf2fbdd2431a63f26b2c86e0078ab6f19aa6
Serhii Vidernikov <sv54@gcloud.ua.es>
2021-11-04 17:48:36 +0100

    WTS GRPC cambios

--- WaitingTimeServer/FWQ_WaitingTimeServer.py
+++ WaitingTimeServer/FWQ_WaitingTimeServer.py
@@ -1,4 +1,5 @@
 from concurrent import futures
+from google.protobuf import message
 from kafka import KafkaConsumer
 import logging
 import grpc
@@ -7,12 +8,20 @@ sys.path.append('C:/Users/serge/source/repos/SD-FWQ/WaitingTimeServer')
 import TimeServer_pb2
 import TimeServer_pb2_grpc
 
-def ObtenerTiempo():
-    channel = grpc.insecure_channel('localhost:50051')
-    #channel = grpc.insecure_channel('192.168.4.246:50051')
-    stub = TimeServer_pb2_grpc.CalculateTimeStub(channel)
-    response = stub.Registry(TimeServer_pb2.RegistryRequest(ID=1,name="you",password="12345"))
-    print("Client received: " + response.response)
+def CalcularTiempo():
+    return 1
+
+class Time(TimeServer_pb2_grpc.CalculateTimeServicer):
+	siguienteUsuario=2
+	def Time(self,request,context):
+		resul=CalcularTiempo()
+		return TimeServer_pb2_grpc.TimeResponse(message=resul)
+# def ObtenerTiempo():
+#     channel = grpc.insecure_channel('localhost:50051')
+#     #channel = grpc.insecure_channel('192.168.4.246:50051')
+#     stub = TimeServer_pb2_grpc.CalculateTimeStub(channel)
+#     response = stub.Registry(TimeServer_pb2.RegistryRequest(ID=1,name="you",password="12345"))
+#     print("Client received: " + response.response)
 # class WaitingTime(object):
 #     def WaitingTimeServer(self,request,context):
 # 		return WaitingTimeServer.WaitingTimeServerResponse(response=calcularTiempo())
@@ -26,17 +35,17 @@ def escuchaSensor():
   	print("hola q tal")
 
 def main():
-	if(len(sys.argv) != 4):
-		print("Para ejecutar utiliza: FWQ_WaitingTimeServer.py |PUERTO ESCUCHA| |IP GESTOR| |PUERTO GESTOR|")
-  	else:
-		puerto_escucha = sys.argv[1]
-		ip_gestor = sys.argv[2]
-		puerto_gestor = sys.argv[3]
-		personas = 0
+	# if(len(sys.argv) != 4):
+	# 	print("Para ejecutar utiliza: FWQ_WaitingTimeServer.py |PUERTO ESCUCHA| |IP GESTOR| |PUERTO GESTOR|")
+  	# else:
+	# 	puerto_escucha = sys.argv[1]
+	# 	ip_gestor = sys.argv[2]
+	# 	puerto_gestor = sys.argv[3]
+	# 	personas = 0
 
-		escuchaSensor()
+	# 	escuchaSensor()
 
-		#escuchaEngine()
+	# 	#escuchaEngine()
 
 
 
