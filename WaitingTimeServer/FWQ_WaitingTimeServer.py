90457592c4117a5b970470c8edd7245a74c8d4ed
Serhii Vidernikov <sv54@gcloud.ua.es>
2021-11-04 18:28:07 +0100

    limpieza

--- WaitingTimeServer/FWQ_WaitingTimeServer.py
+++ WaitingTimeServer/FWQ_WaitingTimeServer.py
@@ -15,20 +15,6 @@ class Time(TimeServer_pb2_grpc.CalculateTimeServicer):
 	def Time(self,request,context):
 		resul=CalcularTiempo()
 		return TimeServer_pb2.TimeResponse(message=str(resul))
-# def ObtenerTiempo():
-#     channel = grpc.insecure_channel('localhost:50051')
-#     #channel = grpc.insecure_channel('192.168.4.246:50051')
-#     stub = TimeServer_pb2_grpc.CalculateTimeStub(channel)
-#     response = stub.Registry(TimeServer_pb2.RegistryRequest(ID=1,name="you",password="12345"))
-#     print("Client received: " + response.response)
-# class WaitingTime(object):
-#     def WaitingTimeServer(self,request,context):
-# 		return WaitingTimeServer.WaitingTimeServerResponse(response=calcularTiempo())
-#     def calcularTiempo():
-
-
-
-
 
 def escuchaSensor(server,puerto):
 	global personas
