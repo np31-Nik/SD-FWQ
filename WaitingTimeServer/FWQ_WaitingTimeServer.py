025def96cb5f3a4bfc39243c015e4dc48c092ecc
Nikita Polyanskiy <np31@alu.ua.es>
2021-11-04 20:02:01 +0100

    waiingtime

--- WaitingTimeServer/FWQ_WaitingTimeServer.py
+++ WaitingTimeServer/FWQ_WaitingTimeServer.py
@@ -8,8 +8,13 @@ sys.path.append('C:/Users/serge/source/repos/SD-FWQ/WaitingTimeServer')
 import TimeServer_pb2
 import TimeServer_pb2_grpc
 import numpy as np
+import time
+import traceback
 
 tiempos = []
+atr = []
+num_atr=0
+
 def CalcularTiempo():
 	message = 'Python is fun'
 	# convert string to bytes
@@ -20,7 +25,7 @@ def CalcularTiempo():
 class Time(TimeServer_pb2_grpc.CalculateTimeServicer):
 	def Time(self,request,context):
 		resul=CalcularTiempo()
-		return TimeServer_pb2_grpc.TimeResponse(message=resul)
+		return TimeServer_pb2.TimeResponse(times=resul)
 # def ObtenerTiempo():
 #     channel = grpc.insecure_channel('localhost:50051')
 #     #channel = grpc.insecure_channel('192.168.4.246:50051')
@@ -34,19 +39,37 @@ class Time(TimeServer_pb2_grpc.CalculateTimeServicer):
 
 def generarTiempos(num_atr,atracciones):
 	global tiempos
-	tiempos = np.full((num_atr,2),0)
+	tiempos = np.full((num_atr,3),0)
 	for i in range(num_atr):
 		tiempos[i][0] = atracciones[i]
 
 	print(tiempos)
 
-def actualizarTiempos(id_atr,num_atr,atr):
+def actualizarTiempos(id_atr,personas,anyadir):
 	global tiempos
 	datos = atr[np.where(atr[:,0] == id_atr)]
 
 	for i in range(num_atr):
 		if tiempos[i][0] == id_atr:
-			print("hola")
+			if anyadir:
+				ciclos = round(personas/datos[1])
+				tiempo = ciclos * datos[2]
+				tiempos[i][1] += datos[2]
+			else:
+				if tiempos[i][1] %
+				tiempos[i][1] -= datos[1]
+
+def every(id,server,puerto):
+	delay = 1
+	next_time = time.time() + delay
+	while True:
+		time.sleep(max(0, next_time - time.time()))
+		try:
+			for i in range(num_atr):
+				actualizarTiempos(i,0,False)
+		except Exception:
+			traceback.print_exc()
+		next_time += delay
 
 			
 def escuchaSensor(server,puerto):
@@ -57,8 +80,8 @@ def escuchaSensor(server,puerto):
     )
 
 	for msg in consumer:
-		#print(msg)
 		datos=msg.value.decode('UTF-8').split(':')
+		actualizarTiempos(datos[0],datos[1],True)
 		print(datos)
 
 def main():
