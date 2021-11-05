bf652403519861cf18c4c44489a2a76727f0576e
Serhii Vidernikov <sv54@gcloud.ua.es>
2021-11-05 20:14:11 +0100

    Visitor, ip y puettos por argumento

--- Visitor/FWQ_Visitor.py
+++ Visitor/FWQ_Visitor.py
@@ -77,37 +77,43 @@ def run():
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
 
-    fila=0
-    columna=0
+    if(len(sys.argv) != 5):
+        print("Para ejecutar utiliza: FWQ_Sensor.py |IP GRPC SERVER| |PUERTO| |IP BROCKER SERVER| |PUERTO|")
+    else:
+        serverGrpc = sys.argv[1]
+        puertoGrpc = sys.argv[2]
+        serverKafka = sys.argv[3]
+        puertoKafa=sys.argv[4]
 
+        fila=0
+        columna=0
 
-    #FALTA RECIBIR POR ARGUMENTO LOS IP Y PUETOS
-    #Elige al azar una de las atracciones con menos de 60 minutos de espera y va hacia ella
-    #Si en el camino cambia a tener 60 minuos o mas elige otra.
-    #Cada segundo dara un paso en una de la 8 direcciones
-    matriz = np.full((20,20), '---')
-    matriz[2][2]='a1'
-    matriz[4][9]='a2'
-    matriz[13][18]='a3'
-    matriz[5][1]='a4'
-    matriz[8][8]='a5'
-    print(matriz)
+        #FALTA RECIBIR POR ARGUMENTO LOS IP Y PUETOS
+        #Elige al azar una de las atracciones con menos de 60 minutos de espera y va hacia ella
+        #Si en el camino cambia a tener 60 minuos o mas elige otra.
+        #Cada segundo dara un paso en una de la 8 direcciones
+        matriz = np.full((20,20), '---')
+        matriz[2][2]='a1'
+        matriz[4][9]='a2'
+        matriz[13][18]='a3'
+        matriz[5][1]='a4'
+        matriz[8][8]='a5'
+        print(matriz)
 
-    opcion=0
-    while(opcion!=1 or opcion!= 2 or opcion!=3):
-        print("Eliga una opcion: \n 1) Registrarse; \n 2) Iniciar sesion; \n 3) Modificar usuario;\n 4) Salir;")
-        opcion = input()
-        if opcion == "1":
-            registarse()
-        if opcion == "2":
-            print(iniciarSesion())
-        if opcion == "3":
-            modificarUsuario()
-        if opcion =="4":
-            break
-    
-    
-    print("Hasta pronto!")
+        opcion=0
+        while(opcion!=1 or opcion!= 2 or opcion!=3):
+            print("Eliga una opcion: \n 1) Registrarse; \n 2) Iniciar sesion; \n 3) Modificar usuario;\n 4) Salir;")
+            opcion = input()
+            if opcion == "1":
+                registarse()
+            if opcion == "2":
+                print(iniciarSesion())
+            if opcion == "3":
+                modificarUsuario()
+            if opcion =="4":
+                break
+        
+        print("Hasta pronto!")
 
 if __name__ == "__main__":
     logging.basicConfig()
