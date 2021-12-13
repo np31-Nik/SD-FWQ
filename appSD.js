7b906f1533c2deb5650f118bd8d5c1b60b4e7edd
Nikita Polyanskiy <np31@alu.ua.es>
2021-12-13 11:55:32 +0100

    GET usuarios

--- appSD.js
+++ appSD.js
@@ -1,12 +1,58 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 // Se define el puerto
 const port=3000;
-app.get("/",(req, res) => {
-res.json({message:'Página de inicio de aplicación de ejemplo de SD'})
-});
+const sqlite3 = require('sqlite3').verbose();
+
 // Ejecutar la aplicacion
 app.listen(port, () => {
-console.log(`Ejecutando la aplicación API REST de SD en el puerto
-${port}`);
+    console.log(`Ejecutando la aplicación API REST de SD en el puerto
+    ${port}`);
 });
+
+// open the database
+let connection = new sqlite3.Database('db.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the database.');
+  });
+
+  app.get("/",(req,res) => {
+    console.log("GET /")
+  });
+
+//usuarios GET
+app.get("/usuarios",(req, response) => {
+    connection.all(`SELECT * FROM usuarios`,[], (err, rows) => {
+    if (err) {
+        response.send(err.message);
+        console.log("Error GET/usuarios")
+    }
+        console.log(rows)
+        response.send(rows)
+  });
+  
+});
+
+
+
+
+
+// db.serialize(() => {
+//   db.each(`SELECT *
+//            FROM usuarios`, (err, row) => {
+//     if (err) {
+//       console.error(err.message);
+//     }
+//     console.log(row.id + "\t" + row.username);
+//   });
+// });
+
+// db.close((err) => {
+//   if (err) {
+//     console.error(err.message);
+//   }
+//   console.log('Close the database connection.');
+// });
