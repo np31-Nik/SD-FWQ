130bb0aba60b7d42504cecb273b1d92651ba89a7
Serhii Vidernikov <sv54@gcloud.ua.es>
2021-12-20 12:50:23 +0100

    Visitor medio arreglado

--- Visitor/FWQ_Visitor.py
+++ Visitor/FWQ_Visitor.py
@@ -166,28 +166,28 @@ def calcularPaso(fila,columna,filaAtraccion, colAtraccion):
 
     if fila<filaAtraccion:
         fila=fila+1
-        if matriz[fila][columna]!='---' and matriz[fila][columna].find('u')==-1:
+        if matriz[fila][columna]=='X':
             #recalcular
             columna=columna+1
             print('recalculando ruta...')
         return fila,columna,False
     elif fila>filaAtraccion:
         fila=fila-1
-        if matriz[fila][columna]!='---' and matriz[fila][columna].find('u')==-1:
+        if matriz[fila][columna]=='X':
             #recalcular
             columna=columna+1
             print('recalculando ruta...')
         return fila,columna,False
     elif columna>colAtraccion:
         columna=columna-1
-        if matriz[fila][columna]!='---' and matriz[fila][columna].find('u')==-1:
+        if matriz[fila][columna]=='X':
             #recalcular
             fila=fila+1
             print('recalculando ruta...')
         return fila,columna,False
     elif columna<colAtraccion:
         columna=columna+1
-        if matriz[fila][columna]!='---' and matriz[fila][columna].find('u')==-1:
+        if matriz[fila][columna]=='X':
             #recalcular
             fila=fila+1
             print('recalculando ruta...')
